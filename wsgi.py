b15a8ad0c332baae7b9a7f6d074ec788b1b6fc15
joao <joaopedro2905@gmail.com>
2025-05-12 11:42:31 -0300

    Deploy inicial para Render

--- wsgi.py
+++ wsgi.py
@@ -2,3 +2,8 @@ from app import app
 
 if __name__ == "__main__":
     app.run()
+https://github.com/joaotchuk/sitema_chamados.git
+
+ git remote add origin https://github.com/joaotchuk/sitema_chamados.git
+git branch -M main
+git push -u origin main
