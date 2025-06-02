2e4a6035995c14c4d72375bbf3f56a0c5302dcd6
joaotchuk <89818403+joaotchuk@users.noreply.github.com>
2025-06-02 17:38:02 -0300

    Update app_with_roles_corrigido.py
    Arruma

--- app_with_roles_corrigido.py
+++ app_with_roles_corrigido.py
@@ -19,8 +19,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'zimbramail.penso.com.br'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'suporte@polico.com.br'  # Altere aqui
-app.config['MAIL_PASSWORD'] = 'Suportepolico2025@'  # Altere aqui
+app.config['MAIL_USERNAME'] = ''  # Altere aqui
+app.config['MAIL_PASSWORD'] = ''  # Altere aqui
 
 mail = Mail(app)
 db = SQLAlchemy(app)
